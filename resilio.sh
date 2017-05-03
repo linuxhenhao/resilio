7cd47a358954021b19c523cea6b2310d4124a10b
huangyu <diwang90@gmail.com>
2017-05-03 14:50:21 +0800

    enhance resilio.sh

--- resilio.sh
+++ resilio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . --label=resilio
-docker run -v /media/u-sda1:/media -p 1999:1999 resilio
+docker build  --no-cache -t resilio ./
+docker run --name resilio_container -v /media/u-sda1:/media -p 1999:1999 resilio
